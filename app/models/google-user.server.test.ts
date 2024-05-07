f2f979ebb574976b6b593880bea6c25bd455d259
Zach Millman <zach.millman@gmail.com>
2024-05-06 21:20:49 -0700

    Add stub for organization linking behavior

---
+++ app/models/google-user.server.test.ts
@@ -0,0 +1,17 @@
+// import { findOrCreateGoogleUser } from "./user.server";
+
+describe("findOrCreateGoogleUser", () => {
+  describe("when user doesn't exist", () => {
+    it.todo("creates new user");
+    it.todo("updates profile info");
+  });
+
+  describe("when user exists", () => {
+    it.todo("returns existing user");
+    it.todo("updates profile info");
+  });
+
+  describe("associates users to organization by email domain", () => {
+    it.todo("works");
+  });
+});
